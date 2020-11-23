808d65a86e75b128f8b8c90dd1107ad325afd56b
William McLendon <wcmclen@sandia.gov>
2020-11-23 12:05:15 -0700

    add a progress bar to the delay counter

--- create_test_issue_batch.sh
+++ create_test_issue_batch.sh
@@ -8,18 +8,38 @@
 #    #gh issue create --title="Test Issue ${thedate:?} ${i:?}" --body="Test Issue"
 #done
 
-delay_min=60
-delay_max=1800
+delay_min=86
+delay_max=864
 
-for (( i=1; i<= 100; ++i)); do
+# 86400 : seconds in 1 day
+time_limit=86400
+
+START=`date +%s`
+while [ $(( $(date +%s) - ${time_limit:?} )) -lt $START ]; do
     delay_secs=$(( $RANDOM % (${delay_max} - ${delay_min}) + ${delay_min}+1 ))
-    
+
     thedate=$(date +"%Y-%m-%d")
-    echo "gh issue create --title='Test Issue ${thedate:?} ${i:?}' --body='Test Issue'"
-    gh issue create --title="Test Issue ${thedate:?} ${i:?}" --body="Test Issue"
-    
+    thetime=$(date +"%H:%M:%S")
+
+    echo "gh issue create --title='Test Issue ${thedate:?} @ ${thetime:?}' --body='Test Issue'"
+    #gh issue create \
+    #    --title="Test Issue ${thedate:?} @ ${thetime:?}" \
+    #    --body="This issue is for testing the auto-closer tool"
+
+    # delay
     echo "sleep $delay_secs"
-    sleep ${delay_secs}
+    START_DELAY=`date +%s`
+    while [ $(( $(date +%s) - ${delay_secs:?} )) -lt $START_DELAY ]; do
+        sleep 10
+        printf "."
+    done
+    printf "\n"
+    printf "\n"
+
 done
+
+echo ""
+echo "Done."
+echo ""
 
 
