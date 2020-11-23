6ee2c0bef9cecca5943040f4ffd319dc1b6d4188
William McLendon <wcmclen@sandia.gov>
2020-11-22 22:59:30 -0700

    add time delay test issue creation

--- create_test_issue_batch.sh
+++ create_test_issue_batch.sh
@@ -3,13 +3,23 @@
 # Generate a bunch of test issues for testing the autocloser action.
 #
 
+#for (( i=1; i<=30; ++i)); do
+#    echo "gh issue create --title='Test Issue ${thedate:?} ${i:?}' --body='Test Issue'"
+#    #gh issue create --title="Test Issue ${thedate:?} ${i:?}" --body="Test Issue"
+#done
 
-thedate=$(date +"%Y-%m-%d")
+delay_min=60
+delay_max=3600
 
-for (( i=1; i<=30; ++i)); do
+for (( i=1; i<= 100; ++i)); do
+    delay_secs=$(( $RANDOM % (${delay_max} - ${delay_min}) + ${delay_min}+1 ))
+    
+    thedate=$(date +"%Y-%m-%d")
     echo "gh issue create --title='Test Issue ${thedate:?} ${i:?}' --body='Test Issue'"
     gh issue create --title="Test Issue ${thedate:?} ${i:?}" --body="Test Issue"
+    
+    echo "sleep $delay_secs"
+    sleep ${delay_secs}
 done
-
 
 
